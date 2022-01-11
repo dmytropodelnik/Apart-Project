3a377d798f30ccdeeb0caa1e69bb8a3466a86f5a
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-01-12 00:22:45 +0200

    Add DbContext and Authorization Helpers

---
+++ CloneBookingAPI/CloneBookingAPI/Services/Helpers/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CloneBookingAPI.Services.Helpers
+{
+    public class AuthOptions
+    {
+		public const string ISSUER = "CloneBooking";
+		public const string AUDIENCE = "CloneBookingUser";
+		const string KEY = "authentification_security_key!qwe123";
+		public const int LIFETIME = 5;
+		public static SymmetricSecurityKey GetSymmetricSecurityKey()
+		{
+			return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+		}
+	}
+}
--- CloneBookingAPI/CloneBookingAPI/Startup.cs
+++ CloneBookingAPI/CloneBookingAPI/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using CloneBookingAPI.Services.Database;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,10 +11,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CloneBookingAPI
@@ -36,27 +39,28 @@ namespace CloneBookingAPI
             services.AddDbContext<CloneBookingDbContext>(options =>
                 options.UseSqlServer(connection));
 
-            services
-                .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                .AddCookie(options =>
-                {
-                    options.LoginPath = new PathString("/Authorization/Login");
-                    options.AccessDeniedPath = new PathString("/Authorization/Login");
-                });
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("OnlyForUkraine", policy =>
-                {
-                    policy.RequireClaim("country", "Ukraine");
-                });
-            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(options =>
+             {
+                 options.RequireHttpsMetadata = false;
+                 options.SaveToken = true;
+                 options.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidIssuer = "ValidIssuer",
+                     ValidAudience = "ValidateAudience",
+                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("IssuerSigningSecretKey")),
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     ClockSkew = TimeSpan.Zero
+                 };
+             });
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CloneBookingAPI", Version = "v1" });
-            });
+            services.AddCors();
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "CloneBookingAPI", Version = "v1" });
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,14 +69,23 @@ namespace CloneBookingAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloneBookingAPI v1"));
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloneBookingAPI v1"));
             }
 
+            app.UseCors(builder =>
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+            );
+
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
