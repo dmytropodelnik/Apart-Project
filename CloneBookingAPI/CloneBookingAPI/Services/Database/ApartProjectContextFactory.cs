2d6333dbb81152a0645f629f320b508a780ff715
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-01-18 11:02:42 +0200

    Add migrations

---
+++ CloneBookingAPI/CloneBookingAPI/Services/Database/ApartProjectContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace CloneBookingAPI.Services.Database
+{
+    public class ApartProjectContextFactory : IDesignTimeDbContextFactory<ApartProjectDbContext>
+    {
+        public ApartProjectDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApartProjectDbContext>();
+
+            // get configuration from file appsettings.json
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+
+            // get connection string from file appsettings.json
+            string connectionString = config.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new ApartProjectDbContext(optionsBuilder.Options);
+        }
+    }
+}
