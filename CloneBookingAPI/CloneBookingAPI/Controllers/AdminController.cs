78ab9911da9c70ec4a2db7ccd05b992a70f9888d
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-01-21 21:17:39 +0200

    Fix bugs

---
+++ CloneBookingAPI/CloneBookingAPI/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CloneBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : Controller
+    {
+        // GET: api/<AdminController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<AdminController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AdminController>
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] string login, string password)
+        {
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(password))
+            {
+                return null;
+            }
+
+            return Ok();
+        }
+
+        // PUT api/<AdminController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AdminController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
