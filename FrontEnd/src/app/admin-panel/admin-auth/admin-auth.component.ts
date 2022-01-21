3d78591a75f178c1cb0b1c3fdfd6fc33ee4451bc
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-01-21 21:40:47 +0200

    Add loginAdmin method

--- FrontEnd/src/app/admin-panel/admin-auth/admin-auth.component.ts
+++ FrontEnd/src/app/admin-panel/admin-auth/admin-auth.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminAuthComponent implements OnInit {
 
+  login: string | undefined;
+  password: string | undefined;
+
   constructor() { }
+
+  loginAdmin() {
+    let user = {
+      login: this.login,
+      password: this.password,
+    };
+
+    fetch('https://localhost:44381/api/admin/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+       },
+      body: JSON.stringify(user),
+    })
+      .then((r) => r.text())
+      .then((data) => {
+        alert(data);
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
 
   ngOnInit(): void {
   }
