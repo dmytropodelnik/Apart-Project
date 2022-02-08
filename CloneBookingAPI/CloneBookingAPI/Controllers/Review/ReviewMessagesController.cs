8f230871a5b422dfbaf5c44252d6ec1f64ad4c71
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-02-08 20:12:16 +0200

    Add new methods

--- CloneBookingAPI/CloneBookingAPI/Controllers/Review/ReviewMessagesController.cs
+++ CloneBookingAPI/CloneBookingAPI/Controllers/Review/ReviewMessagesController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CloneBookingAPI.Services.Database;
+using CloneBookingAPI.Services.Database.Configurations.Review;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace CloneBookingAPI.Controllers.Review
 {
@@ -7,6 +14,152 @@ namespace CloneBookingAPI.Controllers.Review
     [ApiController]
     public class ReviewMessagesController : Controller
     {
+        private readonly ApartProjectDbContext _context;
 
+        public ReviewMessagesController(ApartProjectDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("getmessages")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ReviewMessage>>> GetMessages()
+        {
+            try
+            {
+                return await _context.ReviewMessages.ToListAsync();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+            catch (OperationCanceledException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+        }
+
+        [Route("addreviewmessage")]
+        [HttpPost]
+        public async Task<IActionResult> AddReview([FromBody] ReviewMessage message)
+        {
+            try
+            {
+                if (message is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                ReviewMessage newMessage = new();
+                newMessage.Text = message.Text;
+                _context.ReviewMessages.Add(newMessage);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+        }
+
+        [Route("changereviewbyname")]
+        [HttpPut]
+        public async Task<IActionResult> ChangeReview([FromBody] ReviewMessage message)
+        {
+            try
+            {
+                if (message is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resMessage = await _context.ReviewMessages.FirstOrDefaultAsync(m => m.Text == message.Text);
+                if (resMessage is null)
+                {
+                    return Json(new { code = 400 });
+                }
+                resMessage.Text = message.Text;
+
+                _context.ReviewMessages.Update(resMessage);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+        }
+
+        [Route("changereview")]
+        [HttpPut("{id}")]
+        public async Task<IActionResult> ChangeCountry([FromBody] ReviewMessage message)
+        {
+            try
+            {
+                if (message is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resMessage = await _context.ReviewMessages.FirstOrDefaultAsync(r => r.Id == message.Id);
+                if (resMessage is null)
+                {
+                    return Json(new { code = 400 });
+                }
+                resMessage.Text = message.Text;
+
+                _context.ReviewMessages.Update(resMessage);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+        }
+
+        [Route("deletereview")]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReview([FromBody] ReviewMessage message)
+        {
+            try
+            {
+                if (message is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resMessage = await _context.ReviewMessages.FirstOrDefaultAsync(r => r.Id == message.Id);
+                if (resMessage is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                _context.ReviewMessages.Remove(resMessage);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 400 });
+            }
+        }
     }
 }
