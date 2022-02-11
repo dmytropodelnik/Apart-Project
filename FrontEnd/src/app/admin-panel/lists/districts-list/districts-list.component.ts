eb88401b6a7cb14eaa011c4aa1dd97fbbf8cb354
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-02-11 22:06:07 +0200

    Update admin panel

--- FrontEnd/src/app/admin-panel/lists/districts-list/districts-list.component.ts
+++ FrontEnd/src/app/admin-panel/lists/districts-list/districts-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { District } from 'src/app/models/Location/district.item';
 
+import AuthHelper from '../../../utils/authHelper';
+
 @Component({
   selector: 'app-districts-list',
   templateUrl: './districts-list.component.html',
@@ -9,10 +11,98 @@ import { District } from 'src/app/models/Location/district.item';
 export class DistrictsListComponent implements OnInit {
 
   districts: District[] | null = null;
+  district: string | null = null;
+  checkedDistrict: number | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  addDistrict(): void {
+    let district = {
+      name: this.district,
+    };
+
+    fetch('https://localhost:44381/api/districts/adddistrict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(district),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getDistricts();
+        } else {
+          alert('Adding error!');
+        }
+        this.district = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  editDistrict(): void {
+    let district = {
+      id: this.checkedDistrict,
+      name: this.district,
+    };
+
+    fetch('https://localhost:44381/api/districts/editdistrict', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(district),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getDistricts();
+        } else {
+          alert('Editing error!');
+        }
+        this.district = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  deleteDistrict(): void {
+    let district = {
+      id: this.checkedDistrict,
+      name: this.district,
+    };
+
+    fetch('https://localhost:44381/api/districts/deletedistrict', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(district),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getDistricts();
+        } else {
+          alert('Editing error!');
+        }
+        this.district = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  getDistricts(): void {
     fetch('https://localhost:44381/api/districts/getdistricts', {
       method: 'GET',
     })
@@ -27,6 +117,18 @@ export class DistrictsListComponent implements OnInit {
       .catch((ex) => {
         alert(ex);
       });
+  }
+
+  setDistrict(id: number | null, district: string): void {
+    this.checkedDistrict = id;
+    this.district = district;
+
+    document.getElementById('editButton')?.removeAttribute('disabled');
+    document.getElementById('deleteButton')?.removeAttribute('disabled');
+  }
+
+  ngOnInit(): void {
+    this.getDistricts();
   }
 
 }
--- FrontEnd/src/app/admin-panel/lists/facilities-list/facilities-list.component.ts
+++ FrontEnd/src/app/admin-panel/lists/facilities-list/facilities-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Facility } from 'src/app/models/facility.item';
 
+import AuthHelper from '../../../utils/authHelper';
+
 @Component({
   selector: 'app-facilities-list',
   templateUrl: './facilities-list.component.html',
@@ -9,10 +11,98 @@ import { Facility } from 'src/app/models/facility.item';
 export class FacilitiesListComponent implements OnInit {
 
   facilities: Facility[] | null = null;
+  facility: string | null = null;
+  checkedFacility: number | null = null;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  addFacility(): void {
+    let facility = {
+      name: this.facility,
+    };
+
+    fetch('https://localhost:44381/api/facilities/addfacility', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(facility),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getFacilities();
+        } else {
+          alert('Adding error!');
+        }
+        this.facility = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  editFacility(): void {
+    let facility = {
+      id: this.checkedFacility,
+      name: this.facility,
+    };
+
+    fetch('https://localhost:44381/api/facilities/editfacility', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(facility),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getFacilities();
+        } else {
+          alert('Editing error!');
+        }
+        this.facility = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  deleteFacility(): void {
+    let facility = {
+      id: this.checkedFacility,
+      name: this.facility,
+    };
+
+    fetch('https://localhost:44381/api/facilities/deletefacility', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + AuthHelper.getToken(),
+      },
+      body: JSON.stringify(facility),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        if (data.code === 200) {
+          this.getFacilities();
+        } else {
+          alert('Editing error!');
+        }
+        this.facility = '';
+      })
+      .catch((ex) => {
+        alert(ex);
+      });
+  }
+
+  getFacilities(): void {
     fetch('https://localhost:44381/api/facilities/getfacilities', {
       method: 'GET',
     })
@@ -27,6 +117,18 @@ export class FacilitiesListComponent implements OnInit {
       .catch((ex) => {
         alert(ex);
       });
+  }
+
+  setFacility(id: number | null, facility: string): void {
+    this.checkedFacility = id;
+    this.facility = facility;
+
+    document.getElementById('editButton')?.removeAttribute('disabled');
+    document.getElementById('deleteButton')?.removeAttribute('disabled');
+  }
+
+  ngOnInit(): void {
+    this.getFacilities();
   }
 
 }
