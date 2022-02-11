e4779b4eaeee3deaa92bb2d50cc45ccc1c76a8ea
WickedImpulse <heider.alkateb@gmail.com>
2022-02-11 01:18:02 +0100

    Azure web app deployment

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -5,13 +5,10 @@ import { City } from '../models/Location/city.item';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  card = [
-    {card: 1},{card: 1},{card: 1},{card: 1},{card: 1},
-  ]
+  card = [{ card: 1 }, { card: 1 }, { card: 1 }, { card: 1 }, { card: 1 }];
 
   displayMonths = 2;
   navigation = 'arrows';
@@ -22,34 +19,33 @@ export class HomeComponent implements OnInit {
   cities: City[] | undefined;
 
   slides = [
-    {text:"Educational Consulting", img:"assets/images/21.png"},
-    {text:"University and Higher Education", img:"assets/images/21.png"},
-    {text:"Migration Consulting", img:"assets/images/21.png"},
-    {text:"NAATI Translations", img:"assets/images/21.png"},
-    {text:"English Courses", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
-    {text:"Tax Return", img:"assets/images/21.png"},
+    { text: 'Educational Consulting', img: 'assets/images/21.png' },
+    { text: 'University and Higher Education', img: 'assets/images/21.png' },
+    { text: 'Migration Consulting', img: 'assets/images/21.png' },
+    { text: 'NAATI Translations', img: 'assets/images/21.png' },
+    { text: 'English Courses', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
+    { text: 'Tax Return', img: 'assets/images/21.png' },
   ];
 
   slideConfig = {
-    "slidesToShow": 6,
-    "slidesToScroll": 1,
-    "dots": false,
-    "infinite": true,
-    "arrows": true,
-    "autoplay": true,
-    "responsive": [
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-
-        }
+        },
       },
       {
         breakpoint: 600,
@@ -57,28 +53,25 @@ export class HomeComponent implements OnInit {
           slidesToShow: 1,
           slidesToScroll: 1,
           arrows: true,
-
-
-        }
+        },
       },
-    ]
+    ],
   };
 
   slideConfig1 = {
-    "slidesToShow": 4,
-    "slidesToScroll": 1,
-    "dots": false,
-    "infinite": true,
-    "arrows": true,
-    "autoplay": true,
-    "responsive": [
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-
-        }
+        },
       },
       {
         breakpoint: 600,
@@ -86,20 +79,17 @@ export class HomeComponent implements OnInit {
           slidesToShow: 1,
           slidesToScroll: 1,
           arrows: true,
-
-
-        }
+        },
       },
-    ]
+    ],
   };
 
-
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     fetch(
-      'https://localhost:44381/api/bookingcategories/getcategories', {
+      'https://apartproject.azurewebsites.net/api/bookingcategories/getcategories',
+      {
         method: 'GET',
       }
     )
@@ -115,22 +105,22 @@ export class HomeComponent implements OnInit {
         //alert(err);
       });
 
-      fetch(
-        'https://localhost:44381/api/cities/getcountrycities?country=Ukraine', {
-          method: 'GET',
+    fetch(
+      'https://apartproject.azurewebsites.net/api/cities/getcountrycities?country=Ukraine',
+      {
+        method: 'GET',
+      }
+    )
+      .then((r) => r.json())
+      .then((r) => {
+        if (r.code === 200) {
+          this.cities = r.cities;
+        } else {
+          alert('Cities fetching error!');
         }
-      )
-        .then((r) => r.json())
-        .then((r) => {
-          if (r.code === 200) {
-            this.cities = r.cities;
-          } else {
-            alert('Cities fetching error!');
-          }
-        })
-        .catch((err) => {
-          alert(err);
-        });
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
-
 }
