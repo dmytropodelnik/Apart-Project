7935d20d0b69d3323a78f6916191ce78fb5fb729
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-03-05 18:00:34 +0200

    Add manipulation with favorites on the backend

--- CloneBookingAPI/CloneBookingAPI/Controllers/UserData/FavoritesController.cs
+++ CloneBookingAPI/CloneBookingAPI/Controllers/UserData/FavoritesController.cs
@@ -1,12 +1,16 @@
 ﻿using CloneBookingAPI.Filters;
 using CloneBookingAPI.Services.Database;
+using CloneBookingAPI.Services.Database.Models.Suggestions;
 using CloneBookingAPI.Services.Database.Models.UserProfile;
+using CloneBookingAPI.Services.POCOs;
+using CloneBookingAPI.Services.POCOs.Search;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -106,13 +110,134 @@ namespace CloneBookingAPI.Controllers.UserData
             }
         }
 
-        [Route("addsuggestion")]
-        [HttpPost]
-        public async Task<IActionResult> AddSuggestion([FromBody] Favorite favorite)
+        [Route("getuserfavorites")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Favorite>>> GetUserFavorites(FavoriteSearchPoco items)
         {
             try
             {
+                var suggestions = await _context.Favorites
+                    .Include(f => f.User)
+                    .Include(f => f.Suggestions)
+                        .ThenInclude(s => s.Images)
+                    .Where(f => f.UserId == int.Parse(items.UserId) &&
+                                f.Suggestions
+                                .All(s => s.RoomsAmount > items.RoomAmount &&
+                                          s.ReservedDates
+                                            .Any(s =>
+                                                 s <= items.CheckIn || s >= items.CheckOut)))                   
+                    .ToListAsync();
 
+                return Json(new { 
+                    code = 200, 
+                    suggestions,
+                });
+            }
+            catch (ArgumentNullException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+            catch (OperationCanceledException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+        }
+
+        [Route("addsuggestion")]
+        [HttpPost]
+        public async Task<IActionResult> AddSuggestion([FromBody] SuggestionPoco suggestion)
+        {
+            try
+            {
+                if (suggestion is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resSuggestion = await _context.Suggestions.FirstOrDefaultAsync(s => s.Id == suggestion.Id);
+                if (resSuggestion is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resFavorite = await _context.Favorites.FirstOrDefaultAsync(f => f.UserId == suggestion.UserId);
+                if (resFavorite is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                resFavorite.Suggestions.Add(resSuggestion);
+
+                _context.Favorites.Update(resFavorite);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+            catch (DbUpdateException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+            catch (OperationCanceledException ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+
+                return Json(new { code = 500 });
+            }
+        }
+
+        [Route("removesuggestion")]
+        [HttpDelete]
+        public async Task<IActionResult> RemoveSuggestion([FromBody] SuggestionPoco suggestion)
+        {
+            try
+            {
+                if (suggestion is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resSuggestion = await _context.Suggestions.FirstOrDefaultAsync(s => s.Id == suggestion.Id);
+                if (resSuggestion is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                var resFavorite = await _context.Favorites.FirstOrDefaultAsync(f => f.UserId == suggestion.UserId);
+                if (resFavorite is null)
+                {
+                    return Json(new { code = 400 });
+                }
+
+                resFavorite.Suggestions.Remove(resSuggestion);
+
+                _context.Favorites.Update(resFavorite);
+                await _context.SaveChangesAsync();
+
+                return Json(new { code = 200 });
             }
             catch (DbUpdateConcurrencyException ex)
             {
---
+++ CloneBookingAPI/CloneBookingAPI/Services/POCOs/Search/FavoriteSearchPoco.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CloneBookingAPI.Services.POCOs.Search
+{
+    public class FavoriteSearchPoco
+    {
+        public string UserId { get; set; }
+        public int RoomAmount { get; set; }
+        public int GuestsAmount { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
