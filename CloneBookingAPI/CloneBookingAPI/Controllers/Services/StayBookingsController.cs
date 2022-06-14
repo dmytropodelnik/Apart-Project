8a53a627d8d464dd7199bd758641406f6782c07e
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2022-06-15 00:32:05 +0300

    Update user-bookings page

--- CloneBookingAPI/CloneBookingAPI/Controllers/Services/StayBookingsController.cs
+++ CloneBookingAPI/CloneBookingAPI/Controllers/Services/StayBookingsController.cs
@@ -90,6 +90,7 @@ namespace CloneBookingAPI.Controllers.Services
                     .Include(b => b.Suggestion.BookingCategory)
                     .Include(b => b.BookingStatus)
                     .Include(b => b.CustomerInfo)
+                    .Include(b => b.Guests)
                     .Include(b => b.Price)
                         .ThenInclude(p => p.Currency)
                     .Where(b => b.User.Email.Equals(email) && b.IsRevealed)
